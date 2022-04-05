d0baf468c0f8d0b6e1a0b73d243f38f24173bf2f
Kozyava <48145809+25081998@users.noreply.github.com>
2022-04-06 01:43:57 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    return "Hello, World!"
+
+@app.route("/")
+def index():
+    return "Index page"
+
+if __name__ == "__lab2__":
+    app.run()
